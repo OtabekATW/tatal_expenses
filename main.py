8009f0cbcc9d05fd66ee261a4827ebb0407a34c2
OtabekATW <otabektursunov468@gmail.com>
2023-12-15 18:26:55 +0500

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Calculate the total expenses
+import csv
 def total_expenses(file_name: str) -> int:
     """
     Calculate the total expenses
@@ -7,4 +8,11 @@ def total_expenses(file_name: str) -> int:
     Returns:
         total_expenses: total expenses
     """
-    return 
+    f = open('data.csv')
+    data = csv.reader(f)
+
+    total = 0
+    for row in data:
+        total += int(row[1][:-1])
+
+    return total
